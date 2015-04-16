24eb29ffd1ed46ff44afa6d92c8526e9207fec2d
Ahlisen <ahlisen93@gmail.com>
2015-04-17 00:05:58 +0200

    battlechanges

--- app/js/arenaCtrl.js
+++ app/js/arenaCtrl.js
@@ -1,22 +1,51 @@
 // User controller that we use whenever we want to display detailed
 // information about a user
-companionApp.controller('ArenaCtrl', function ($scope,$routeParams,$firebaseObject,Companion,$rootScope) {
+companionApp.controller('ArenaCtrl', function ($scope,$routeParams,$firebaseObject,Companion,$rootScope,$timeout) {
 
+  $scope.combo = 1;
+  $scope.maxTimer = 10;
+  $scope.battle = false;
+  $scope.timer = $scope.maxTimer;
   var ref = new Firebase("https://companion-simulation.firebaseio.com");
   var userRef = new Firebase("https://companion-simulation.firebaseio.com/users/"+$rootScope.user.uid);
 
+//var myVar2=setInterval(function () {reduceTime()}, 1000);
+
+$scope.startCombat = function() {
+  $scope.combo = 1;
+  var random = Math.floor((Math.random() * 718) + 1);
+  getSpecificPokemon(random);
+  $scope.battle = true;
+}
+
+
+function reduceTime() {
+  if ($scope.timer>0){
+    //console.log($scope.timer);
+    $scope.timer = $scope.timer - 1;
+  }
+  else
+  {
+    takeDmg();
+    $scope.timer = 10;
+  }
+
+  if($scope.battle){
+    $timeout( function(){ reduceTime(); }, 200);
+  }
+}
+
   // Get pokemon data
   var getPokemon = function() {
-    $scope.status = "Loading...";
     // console.log("pokemon: "+$scope.user.pokemon);
     if($scope.user.pokemon === 'egg') {
-      $scope.sprite = 'images/egg_jump.gif';
-      $scope.status = "";
+      $scope.pokemon.sprite = 'images/egg_jump.gif';
     }
     else {
-      $scope.status = "";
+      console.log("WE GOT A MONSTER");
       $scope.pokemon = $scope.user.pokemon;
       $scope.pokemon.curHp = $scope.pokemon.hp;
+
       // Companion.pokemon.get({id:$scope.user.pokemon}, function(data){
       //     $scope.status = "";
       //     $scope.pokemon = data;
@@ -29,6 +58,22 @@ companionApp.controller('ArenaCtrl', function ($scope,$routeParams,$firebaseObje
     }
   }
 
+  var takeDmg = function() {
+    $scope.combo = 1;
+    var random2 = Math.floor((Math.random() * 10) + 1);
+    $scope.pokemon.curHp = Math.max(0,$scope.pokemon.curHp-Math.floor(($scope.temp_monster.attack*random2)/$scope.pokemon.defense));
+
+    if ($scope.pokemon.curHp<=0){
+      console.log("Random id will be: "+ randomPoke);
+
+      userRef.update({ //Gör väldigt många calls i consolen när den dör
+              pokemon: 'egg',
+              lvl: 0
+            });
+      window.location.href = '#/home';
+    }
+  }
+
   $scope.attackEnemy = function() {
 
     var randomMonster = Math.floor((Math.random() * 500) + 1);
@@ -36,12 +81,22 @@ companionApp.controller('ArenaCtrl', function ($scope,$routeParams,$firebaseObje
     var random1 = Math.floor((Math.random() * 10) + 1);
     var random2 = Math.floor((Math.random() * 10) + 1);
 
-    if ($scope.temp_monster.hp === 0) {
+    if ($scope.temp_monster.curHp === 0) {
       return
     } else {
-      $scope.temp_monster.hp = Math.max(0,$scope.temp_monster.hp-Math.floor(($scope.pokemon.attack*random1)/$scope.temp_monster.defense));
+
+      if ($scope.timer <= 3 && $scope.timer >= 1){
+        $scope.combo += 1;
+      }
+      else{
+        $scope.combo = 1;
+      }
+      $scope.timer = 10;
+
+      $scope.temp_monster.curHp = Math.max(0,$scope.temp_monster.curHp-Math.floor(($scope.pokemon.attack*random1*$scope.combo)/$scope.temp_monster.defense));
       $scope.pokemon.curHp = Math.max(0,$scope.pokemon.curHp-Math.floor(($scope.temp_monster.attack*random2)/$scope.pokemon.defense));
-      if ($scope.temp_monster.hp<=0) {
+      if ($scope.temp_monster.curHp<=0) {
+        $scope.battle = false;
         $scope.pokemon.exp += 30;
         $scope.pokemon.hp += Math.floor(Math.random()*21)-10; //Add or subtract hp, very exciting
         userRef.child("pokemon").update({
@@ -52,11 +107,11 @@ companionApp.controller('ArenaCtrl', function ($scope,$routeParams,$firebaseObje
         //getSpecificPokemon(randomMonster);
         $scope.pokemon.curHp = $scope.pokemon.hp;
       }
+
     }
 
     if ($scope.pokemon.curHp<=0){
-        console.log("Random id will be: "+ randomPoke);
-
+        $scope.battle = false;
         userRef.update({ //Gör väldigt många calls i consolen när den dör
                 pokemon: 'egg',
                 lvl: 0
@@ -81,7 +136,9 @@ companionApp.controller('ArenaCtrl', function ($scope,$routeParams,$firebaseObje
         $scope.temp_monster = data;
         console.log($scope.temp_monster);
         getSprite($scope.temp_monster);
+        $scope.temp_monster.curHp = $scope.temp_monster.hp;
         //console.log("TEMP_MONSTER_IMG: "+temp_monster.species);
+        reduceTime(); //START MORTAL COMBAT
 
       }, function(data){
         $scope.status = "There was an error";
@@ -109,8 +166,6 @@ companionApp.controller('ArenaCtrl', function ($scope,$routeParams,$firebaseObje
     $scope.user = snapshot.val();
     console.log($scope.user);
     getPokemon();
-    var random = Math.floor((Math.random() * 718) + 1);
-    getSpecificPokemon(random);
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
