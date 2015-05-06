c5763056cab400c621a0855fc694f1cb977a410c
apan1000 <fredrik.911225@gmail.com>
2015-05-07 00:23:21 +0200

    Not beginner after tutorial

--- app/js/tutorialCtrl.js
+++ app/js/tutorialCtrl.js
@@ -1,19 +1,18 @@
 //Tutorial Ctrl for the Tutorial which is displayed when a user is new.
 companionApp.controller('TutorialCtrl', function ($scope,$routeParams,$firebaseObject,Companion,$rootScope,$timeout) {
-	$scope.user = Companion.getUser();
-
-  	var ref = new Firebase("https://companion-simulation.firebaseio.com");
-
-	if ($routeParams.user) {
-		var otherUserRef = ref.child('users/'+$routeParams.user);
-	} else {
-		var userRef = ref.child('users/'+Companion.getUser().uid);
-	}
 
 	if($scope.user.beginner === true) {
 		$scope.showTutorial = true;
 		$scope.welcome = true; 
 	}
+
+  $scope.endTutorial = function() {
+    $timeout(function() {
+      $scope.showTutorial = false;
+      $scope.user.beginner = false;
+      Companion.setUser($scope.user);
+    });
+  }
 
 	$scope.showStepOne = function() {
     	$scope.welcome = false; 
