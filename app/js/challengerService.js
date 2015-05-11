1308d6de8be25de6b5849987efe107a680ce3d60
Ahlisen <ahlisen93@gmail.com>
2015-05-11 22:50:32 +0200

    online barebones, not working

--- app/js/challengerService.js
+++ app/js/challengerService.js
@@ -3,28 +3,45 @@ companionApp.factory('Challenger', function (Companion,$rootScope,$routeParams,$
 	var self = this;
 	var challenger = null;
 	var ref = new Firebase("https://companion-simulation.firebaseio.com");
+	var userRef = new Firebase("https://companion-simulation.firebaseio.com/users/"+$rootScope.user.uid);
 	var battleRef = {};
 	
-	this.startBattle = function() {
+	this.initialize = function() {
 		ref.once('value', function(snapshot) {
 			if (snapshot.hasChild("battles/"+$routeParams.battleID)) {
 				battleRef = ref.child("battles/"+$routeParams.battleID);
 				// Get challenger from Firebase
-			  battleRef.once('value', function(snapshot) {
-			    if (snapshot.val()) {
-			      console.log(snapshot.val());
-			    }
-			  });
-			} else {
+			  	battleRef.once('value', function(snapshot) {
+				    if (snapshot.val()) {
+				      console.log(snapshot.val());
+				    }
+			  	});
+			}
+			else
+			{
 				$timeout(function() {
-	        $location.path("/home");
-	      });
+	        		$location.path("/home");
+	      		});
 			}
 		});
 	}
 
+	this.getChallengerCont = function() {
+		otherUserRef.on("value", function(snapshot) {
+	      $timeout(function() {
+	        challenger = snapshot.val();
+	        console.log("otherPlayer:",challenger);
+	      });
+	    });
+		return challenger
+	}
+
 	this.getChallenger = function() {
 		return challenger
+	}
+
+	this.makeMove = function() {
+
 	}
 
 	return this
